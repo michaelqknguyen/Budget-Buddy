108e91d76122277fd429d731ff7fea3e37b7c4f8
Michael Nguyen <michaelqknguyen@gmail.com>
2022-07-24 19:58:30 -0400

    setting output field type

--- budgetbuddy/pages/views.py
+++ budgetbuddy/pages/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.db.models import Sum, F, Q
+from django.db.models import Sum, F, Q, DecimalField
 from django.db.models.functions import Coalesce
 from django.urls import reverse
 from budgetbuddy.accounts.models import MoneyAccount, BudgetAccount
@@ -22,7 +22,7 @@ def index(request):
             active=True,
             user=user
         )
-        .annotate(total=Coalesce(Sum(F('transaction__amount_spent')), 0))
+        .annotate(total=Coalesce(Sum(F('transaction__amount_spent'), output_field=DecimalField()), 0))
         .aggregate(money_total=Coalesce(Sum('total'), 0))
     ).get('money_total')
 
@@ -33,7 +33,7 @@ def index(request):
             active=True,
             user=user
         )
-        .annotate(total=Coalesce(Sum(F('transaction__amount_spent')), 0))
+        .annotate(total=Coalesce(Sum(F('transaction__amount_spent'), output_field=DecimalField()), 0))
         .aggregate(balance_total=Coalesce(Sum('total'), 0))
     ).get('balance_total')
 
@@ -43,7 +43,7 @@ def index(request):
         flex_account = (
             BudgetAccount
             .objects
-            .annotate(total=Coalesce(Sum(F('transaction__amount_spent')), 0))
+            .annotate(total=Coalesce(Sum(F('transaction__amount_spent'), output_field=DecimalField()), 0))
             .get(
                 Q(account_type__account_type='Flex'),
                 user=user,
